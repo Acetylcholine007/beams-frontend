26d0b13b27c8c20f99e52336cab305929ce8e576
Ellen <ellen@Ellens-MacBook-Air.local>
2022-06-04 20:40:22 +0800

    Revised NodeDetails layout

--- src/views/dashboardView/pages/NodeDetailsPage.jsx
+++ src/views/dashboardView/pages/NodeDetailsPage.jsx
@@ -1,6 +1,8 @@
 import {
   Box,
   Card,
+  CardContent,
+  CardHeader,
   Checkbox,
   CircularProgress,
   Container,
@@ -19,7 +21,7 @@ const NodeDetailsPage = () => {
 
   return (
     <Container sx={{ height: "100%" }}>
-      <Box sx={{ height: "50%", display: "flex", flexDirection: "column" }}>
+      <Box sx={{ height: "80%", display: "flex", flexDirection: "column" }}>
         <Stack
           direction="row"
           justifyContent="stretch"
@@ -30,49 +32,87 @@ const NodeDetailsPage = () => {
             elevation={4}
             sx={{
               height: "100%",
-              flexGrow: "1",
-              padding: 2,
+              flexBasis: "50%",
+              display: "flex",
+              flexDirection: "column",
             }}
           >
-            {readings && (
-              <LineChart
-                data={[
-                  { name: "X", data: readings.rawX },
-                  { name: "Y", data: readings.rawY },
-                  { name: "Z", data: readings.rawZ },
-                ]}
-                xAxis={readings.rawDatetime}
-                height={"100%"}
-                width={"100%"}
-              />
-            )}
-            {!readings && <CircularProgress />}
+            <CardHeader
+              title={
+                <Typography variant="h6" color="white">
+                  Realtime Raw Accelerometer Readings
+                </Typography>
+              }
+              sx={{ backgroundColor: "primary.main" }}
+            />
+            <CardContent
+              sx={{
+                flexGrow: "1",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "stretch",
+              }}
+            >
+              {readings && (
+                <LineChart
+                  data={[
+                    { name: "X", data: readings.rawX },
+                    { name: "Y", data: readings.rawY },
+                    { name: "Z", data: readings.rawZ },
+                  ]}
+                  xAxis={readings.rawDatetime}
+                  height={"100%"}
+                  width={"100%"}
+                />
+              )}
+              {!readings && <CircularProgress sx={{ alignSelf: "center" }} />}
+            </CardContent>
           </Card>
           <Card
             elevation={4}
             sx={{
               height: "100%",
-              flexGrow: "1",
-              padding: 2,
+              flexBasis: "50%",
+              display: "flex",
+              flexDirection: "column",
             }}
           >
-            {readings && (
-              <LineChart
-                data={[
-                  { name: "X", data: readings.rawX },
-                  { name: "Y", data: readings.rawY },
-                  { name: "Z", data: readings.rawZ },
-                ]}
-                xAxis={readings.rawDatetime}
-                height={"100%"}
-                width={"100%"}
-              />
-            )}
-            {!readings && <CircularProgress />}
+            <CardHeader
+              title={
+                <Typography variant="h6" color="white">
+                  Accelerometer Raw Reading Snapshot
+                </Typography>
+              }
+              sx={{ backgroundColor: "primary.main" }}
+            />
+            <CardContent
+              sx={{
+                flexGrow: "1",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "stretch",
+              }}
+            >
+              {readings && (
+                <LineChart
+                  data={[
+                    { name: "X", data: readings.rawX },
+                    { name: "Y", data: readings.rawY },
+                    { name: "Z", data: readings.rawZ },
+                  ]}
+                  xAxis={readings.rawDatetime}
+                  height={"100%"}
+                  width={"100%"}
+                />
+              )}
+              {!readings && <CircularProgress sx={{ alignSelf: "center" }} />}
+            </CardContent>
           </Card>
         </Stack>
       </Box>
-      <Box sx={{ height: "50%", display: "flex", flexDirection: "column" }}>
+      <Box sx={{ height: "80%", display: "flex", flexDirection: "column" }}>
         <Stack
           direction="row"
           justifyContent="stretch"
@@ -83,152 +123,86 @@ const NodeDetailsPage = () => {
             elevation={4}
             sx={{
               height: "100%",
-              flexGrow: "1",
-              padding: 2,
+              flexBasis: "50%",
+              display: "flex",
+              flexDirection: "column",
             }}
           >
-            {readings && (
-              <LineChart
-                data={[
-                  { name: "X", data: readings.rawX },
-                  { name: "Y", data: readings.rawY },
-                  { name: "Z", data: readings.rawZ },
-                ]}
-                xAxis={readings.rawDatetime}
-                height={"100%"}
-                width={"100%"}
-              />
-            )}
-            {!readings && <CircularProgress />}
+            <CardHeader
+              title={
+                <Typography variant="h6" color="white">
+                  Realtime FFT Accelerometer Readings
+                </Typography>
+              }
+              sx={{ backgroundColor: "primary.main" }}
+            />
+            <CardContent
+              sx={{
+                flexGrow: "1",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "stretch",
+              }}
+            >
+              {readings && (
+                <LineChart
+                  data={[
+                    { name: "X", data: readings.rawX },
+                    { name: "Y", data: readings.rawY },
+                    { name: "Z", data: readings.rawZ },
+                  ]}
+                  xAxis={readings.rawDatetime}
+                  height={"100%"}
+                  width={"100%"}
+                />
+              )}
+              {!readings && <CircularProgress sx={{ alignSelf: "center" }} />}
+            </CardContent>
           </Card>
           <Card
             elevation={4}
             sx={{
               height: "100%",
-              flexGrow: "1",
-              padding: 2,
+              flexBasis: "50%",
+              display: "flex",
+              flexDirection: "column",
             }}
           >
-            {readings && (
-              <LineChart
-                data={[
-                  { name: "X", data: readings.fftX },
-                  { name: "Y", data: readings.fftY },
-                  { name: "Z", data: readings.fftZ },
-                ]}
-                xAxis={readings.fftDatetime}
-                height={"100%"}
-                width={"100%"}
-              />
-            )}
-            {!readings && <CircularProgress />}
+            <CardHeader
+              title={
+                <Typography variant="h6" color="white">
+                  Accelerometer FFT Reading Snapshot
+                </Typography>
+              }
+              sx={{ backgroundColor: "primary.main" }}
+            />
+            <CardContent
+              sx={{
+                flexGrow: "1",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "stretch",
+              }}
+            >
+              {readings && (
+                <LineChart
+                  data={[
+                    { name: "X", data: readings.fftX },
+                    { name: "Y", data: readings.fftY },
+                    { name: "Z", data: readings.fftZ },
+                  ]}
+                  xAxis={readings.fftDatetime}
+                  height={"100%"}
+                  width={"100%"}
+                />
+              )}{" "}
+              {!readings && <CircularProgress sx={{ alignSelf: "center" }} />}
+            </CardContent>
           </Card>
         </Stack>
       </Box>
-      {/* <Grid container spacing={4} sx={{height: '100%'}}>
-        <Grid item xs={12} sx={{height: '100%'}}>
-          <Typography variant="h3">Accelerometer Raw Chart</Typography>
-          <Typography variant="p">Accelerometer raw data readings</Typography>
-        </Grid>
-        <Grid item xs={12} md={8}>
-          <Card
-            elevation={4}
-            sx={{
-              height: "30rem",
-              padding: 2,
-            }}
-          >
-            {readings && (
-              <LineChart
-                data={[
-                  { name: "X", data: readings.rawX },
-                  { name: "Y", data: readings.rawY },
-                  { name: "Z", data: readings.rawZ },
-                ]}
-                xAxis={readings.rawDatetime}
-                height={"100%"}
-                width={"100%"}
-              />
-            )}
-            {!readings && <CircularProgress />}
-          </Card>
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <Typography variant="h6">Controls</Typography>
-          <FormGroup>
-            <FormControlLabel
-              control={
-                <Checkbox defaultChecked checked={true} onChange={() => {}} />
-              }
-              label="Display X - Axis"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox defaultChecked checked={true} onChange={() => {}} />
-              }
-              label="Display Y - Axis"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox defaultChecked checked={true} onChange={() => {}} />
-              }
-              label="Display Z - Axis"
-            />
-          </FormGroup>
-        </Grid>
-        <Grid item xs={12}>
-          <Typography variant="h3">Accelerometer FFT Chart</Typography>
-          <Typography variant="p">
-            Accelerometer fast fourier transform data
-          </Typography>
-        </Grid>
-        <Grid item xs={12} md={8}>
-          <Card
-            elevation={4}
-            sx={{
-              height: "30rem",
-              padding: 2,
-            }}
-          >
-            {readings && (
-              <LineChart
-                data={[
-                  { name: "X", data: readings.fftX },
-                  { name: "Y", data: readings.fftY },
-                  { name: "Z", data: readings.fftZ },
-                ]}
-                xAxis={readings.fftDatetime}
-                height={"100%"}
-                width={"100%"}
-              />
-            )}
-            {!readings && <CircularProgress />}
-          </Card>
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <Typography variant="h6">Controls</Typography>
-          <FormGroup>
-            <FormControlLabel
-              control={
-                <Checkbox defaultChecked checked={true} onChange={() => {}} />
-              }
-              label="Display X - Axis"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox defaultChecked checked={true} onChange={() => {}} />
-              }
-              label="Display Y - Axis"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox defaultChecked checked={true} onChange={() => {}} />
-              }
-              label="Display Z - Axis"
-            />
-          </FormGroup>
-        </Grid>
-      </Grid> */}
     </Container>
   );
 };
